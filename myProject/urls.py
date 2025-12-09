bd2b88c2bff5f8ed3dd923542b96ecc15b803062
Abdul Git <abdulmaaji360@gmail.com>
2025-12-09 13:49:50 +0100

    Removed todo app

--- myProject/urls.py
+++ myProject/urls.py
@@ -5,12 +5,5 @@ from home import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('credits/', home_views.credits, name='credits'),
-    path('about/', home_views.about, name='about'),
-    path('version-info/', home_views.version_info, name='version_info'),
-    path('news/', home_views.news, name='news'),
-    path('news-test/', home_views.news_test, name='news_test'),
-    path('blog/', home_views.blog_home, name='blog_home'),
     path('todo/', include('todo.urls')),
-    path('bands/', include('bands.urls')),
 ]
