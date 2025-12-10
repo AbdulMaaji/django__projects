ea981cd8e5348688bfc1a12e992b6d230d76bd61
Abdul Git <abdulmaaji360@gmail.com>
2025-12-10 17:47:50 +0100

    Posts added

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    tiltle = models.CharField(max_length=75)
+    title = models.CharField(max_length=75)
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
