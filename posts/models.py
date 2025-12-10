a3c9f30b6a15f640e9d7f0ec4b172c24583b2ebb
Abdul Git <abdulmaaji360@gmail.com>
2025-12-10 07:45:24 +0100

    Modelled post

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Post(models.Model):
     tiltle = models.CharField(max_length=75)
-    body
-    slug
-    date
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateField(auto_now_add=True)
